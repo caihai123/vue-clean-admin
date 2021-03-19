daad5c53d188e995510af24b35d49c59b1e62928
caihai <ch15519060282@163.com>
2021-03-19 18:14:28 +0800

    update

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import "@/permissions.js"; // 路由权限
 import "element-ui/lib/theme-chalk/index.css";
 import "element-ui/lib/theme-chalk/display.css";
 import "@/assets/theme/index.css"; //换肤样式
-import "@/assets/style/index.css"; //全局公共样式
+import "@/assets/style/index.scss"; //全局公共样式
 import Fragment from "vue-fragment"; // 可产生虚拟根节点
 import ElTableEdit from "el-table-edit";
 
