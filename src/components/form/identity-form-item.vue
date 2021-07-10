5a25d138ee96b770a59725ec2b54a97cd6e10a9f
蔡海 <ch15519060282@163.com>
2021-07-11 02:12:34 +0800

    代码优化

--- src/components/form/identity-form-item.vue
+++ src/components/form/identity-form-item.vue
@@ -4,12 +4,13 @@
     :rules="[
       {
         required: required,
-        message: '请输入身份证号',
+        message: `请输入${label}`,
         trigger: 'blur',
       },
       {
+        // 正则出处：https://any86.github.io/any-rule/
         pattern: /(^\d{8}(0\d|10|11|12)([0-2]\d|30|31)\d{3}$)|(^\d{6}(18|19|20)\d{2}(0[1-9]|10|11|12)([0-2]\d|30|31)\d{3}(\d|X|x)$)/,
-        message: '请输入正确的身份证号',
+        message: `请输入正确的${label}`,
         trigger: 'blur',
       },
     ]"
--- src/components/form/phone-form-item.vue
+++ src/components/form/phone-form-item.vue
@@ -4,12 +4,13 @@
     :rules="[
       {
         required: required,
-        message: '请输入手机号',
+        message: `请输入${label}`,
         trigger: 'blur',
       },
       {
+        // 正则出处：https://any86.github.io/any-rule/
         pattern: /^(?:(?:\+|00)86)?1[3-9]\d{9}$/,
-        message: '请输入正确的手机号',
+        message: `请输入正确的${label}`,
         trigger: 'blur',
       },
     ]"
