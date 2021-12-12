456e90e357de8ed1b059e29dab029f9a16a2a08f
蔡海 <ch15519060282@163.com>
2021-12-12 21:36:03 +0800

    【更新】用户表

--- src/views/permis/Account.vue
+++ src/views/permis/Account.vue
@@ -1,137 +1,135 @@
 <template>
-  <ElTableEdit :data="tableData" :columns="columns" border style="width: 100%">
-    <el-button
-      slot="header"
-      size="small"
-      type="primary"
-      icon="el-icon-document-add"
-    >
-      新增
-    </el-button>
-    <template v-slot:column-actions="scope">
-      <el-button
-        type="danger"
-        size="small"
-        @click="delItem(scope.row, scope.$index)"
-      >
-        删除
-      </el-button>
-    </template>
-  </ElTableEdit>
+  <div>
+    <el-form inline>
+      <el-form-item>
+        <el-input v-model="params.nickname" placeholder="姓名" />
+      </el-form-item>
+      <el-form-item>
+        <el-input v-model="params.username" placeholder="用户名" />
+      </el-form-item>
+      <el-form-item>
+        <el-input
+          v-model="params.phoneNumber"
+          placeholder="手机号"
+          maxlength="11"
+        />
+      </el-form-item>
+      <el-form-item>
+        <el-select v-model="params.roleId" placeholder="角色">
+          <el-option
+            v-for="item in roleOptions"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          >
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-select v-model="params.status" placeholder="状态">
+          <el-option label="启用" :value="true"> </el-option>
+          <el-option label="禁用" :value="false"> </el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="getTableData(1)">查 询</el-button>
+      </el-form-item>
+    </el-form>
+    <div>
+      <el-table :data="tableData" border>
+        <el-table-column prop="avatar" label="avatar"> </el-table-column>
+        <el-table-column prop="nickname" label="nickname"> </el-table-column>
+        <el-table-column prop="username" label="username"> </el-table-column>
+        <el-table-column prop="phoneNumber" label="phone"> </el-table-column>
+        <el-table-column label="roleId">
+          <template slot-scope="{ row }">
+            {{ roleParse(row.roleId) }}
+          </template>
+        </el-table-column>
+        <el-table-column label="status">
+          <template slot-scope="{ row }">
+            <el-switch v-model="row.status"> </el-switch>
+          </template>
+        </el-table-column>
+        <el-table-column label="action">
+          <template>
+            <el-button type="primary" plain size="mini">编辑</el-button>
+            <el-button type="danger" size="mini">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <div style="margin-top:20px;text-align:right">
+        <el-pagination
+          :total="pagination.total"
+          :page-size="pagination.pageSize"
+          :current-page="pagination.page"
+          @current-change="getTableData"
+        >
+        </el-pagination>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import axios from "@/utils/axios";
+
 export default {
   name: "Account",
-  components: {},
   data() {
     return {
-      columns: [
-        {
-          title: "关联角色",
-          key: "roleName",
-          width: 100,
-        },
-        {
-          title: "用户名",
-          key: "userName",
-          edit: true,
-          rules: [
-            { required: true, message: "请输入活动名称", trigger: "blur" },
-            {
-              min: 3,
-              max: 5,
-              message: "长度在 3 到 5 个字符",
-              trigger: "blur",
-            },
-          ],
-          clearable: true,
-        },
-        {
-          title: "用户密码",
-          key: "password",
-          edit: true,
-          clearable: true,
-        },
-        {
-          title: "姓名",
-          key: "name",
-          edit: true,
-          clearable: true,
-        },
-        {
-          title: "性别",
-          key: "sex",
-          type: "select",
-          options: [
-            {
-              label: "未知",
-              value: "0",
-            },
-            {
-              label: "男",
-              value: "1",
-            },
-            {
-              label: "女",
-              value: "2",
-            },
-          ],
-          edit: true,
-          clearable: true,
-        },
-        {
-          title: "联系电话",
-          key: "phone",
-          edit: true,
-          clearable: true,
-        },
-        {
-          title: "邮箱",
-          key: "email",
-          edit: true,
-          clearable: true,
-        },
-      ],
-      tableData: [
-        {
-          id: "11111",
-          userName: "admin",
-          password: "123456",
-          name: "蔡海",
-          sex: "男",
-          phone: "155********",
-          email: "ch155********@163.com",
-          roleId: "",
-          roleName: "admin",
-        },
-        {
-          id: "22222",
-          userName: "admin",
-          password: "123456",
-          name: "蔡海",
-          sex: "男",
-          phone: "155********",
-          email: "ch155********@163.com",
-          roleId: "",
-          roleName: "admin",
-        },
-      ],
+      params: {
+        nickname: "",
+        username: "",
+        phoneNumber: "",
+        roleId: "",
+        status: "",
+      },
+      roleOptions: [],
+      pagination: {
+        page: 1,
+        pageSize: 10,
+        total: 0,
+      },
+      tableData: [],
     };
   },
-  computed: {},
-  watch: {},
-  created() {},
-  mounted() {},
+  created() {
+    axios("select_role").then((value) => {
+      this.roleOptions = value.data || [];
+      this.getTableData();
+    });
+  },
   methods: {
+    // 获取表格列表
+    getTableData(page) {
+      axios("get_user_list", {
+        page: page || this.pagination.page,
+        pageSize: this.pagination.pageSize,
+        params: this.params,
+      }).then((value) => {
+        const data = value.data;
+        this.tableData = data.list || [];
+        this.pagination.page = data.page;
+        this.pagination.total = data.total;
+      });
+    },
     // 新增用户
     addUserItem() {},
     delItem(row, index) {
       console.log(row);
       console.log(index);
     },
+    // 将roleId转roleName
+    roleParse: function(value) {
+      let found = this.roleOptions.find((item) => item.value === value);
+      if (found) {
+        return found.label;
+      } else {
+        return value;
+      }
+    },
   },
 };
 </script>
-
-<style scoped></style>
