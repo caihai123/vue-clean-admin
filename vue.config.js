9fc6ae9dd9b234d7ddc128dfe070edcd666ceb5d
蔡海 <ch15519060282@163.com>
2022-06-26 23:50:08 +0800

    【新增】浏览器控制打印项目信息

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,29 @@
+const commit = require("./commit.js");
+
+// 打包时间（启动时间）
+const nowDate = new Date();
+const buildDate = `${nowDate.getFullYear() +
+  "-" +
+  (nowDate.getMonth() + 1) +
+  "-" +
+  nowDate.getDate() +
+  " " +
+  nowDate.getHours() +
+  ":" +
+  nowDate.getMinutes() +
+  ":" +
+  nowDate.getSeconds()}`;
+
 module.exports = {
   filenameHashing: true, //是否在文件名中包含了 hash 以便更好的控制缓存
+  chainWebpack: (config) => {
+    config.plugin("define").tap((args) => {
+      args[0]["process.env"].commitHash = JSON.stringify(commit.commitHash);
+      args[0]["process.env"].branchName = JSON.stringify(commit.branchName);
+      args[0]["process.env"].buildDate = JSON.stringify(buildDate);
+      return args;
+    });
+  },
   devServer: {
     https: false,
     port: 1234,
